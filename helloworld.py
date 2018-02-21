3f42a1905f5a4f16e874923de3d5c9e3bc284653
Chris Angelico <rosuav@gmail.com>
2018-02-21 16:44:36 +1100

    Add a hit counter to test events and clocks

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,28 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.clock import Clock
 
-class HelloWorld(App):
+class HelloApp(App):
+	def __init__(self):
+		self.hitcount = 0
+		super().__init__()
+		Clock.schedule_interval(self.waiting, 15)
+
 	def build(self):
-		return Label(text="Hello, world!")
+		return Label(text="Hit me.")
+
+	def on_touch_down(self, touch):
+		if not self.collide_point(*touch.pos): return
+		if self.hitcount:
+			self.hitcount += 1
+			msg = "You've hit me %d times." % self.hitcount
+		else:
+			self.hitcount = 1
+			msg = "Hit me again!"
+		self.root.text = msg
+
+	def waiting(self, delay):
+		self.root.text = "Go on, hit me!"
 
 if __name__ == "__main__":
-	HelloWorld().run()
+	HelloApp().run()
