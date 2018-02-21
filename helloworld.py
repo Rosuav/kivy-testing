c2682fc06a9fd3a4045e77abb883743b7d6acc21
Chris Angelico <rosuav@gmail.com>
2018-02-21 16:54:09 +1100

    Use a button instead of a label

--- helloworld.py
+++ helloworld.py
@@ -1,29 +1,30 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.button import Button
 from kivy.clock import Clock
 
 class HelloApp(App):
 	def __init__(self):
 		self.hitcount = 0
 		super().__init__()
-		Clock.schedule_interval(self.waiting, 15)
+		Clock.schedule_once(self.waiting, 15)
 
 	def build(self):
-		return Label(text="Hit me.")
+		self.btn = Button(text="Hit me.")
+		self.btn.bind(on_press=self.on_click)
+		return self.btn
 
-	def on_touch_down(self, touch):
-		if not self.collide_point(*touch.pos): return
+	def on_click(self, btn):
 		if self.hitcount:
 			self.hitcount += 1
 			msg = "You've hit me %d times." % self.hitcount
 		else:
 			self.hitcount = 1
 			msg = "Hit me again!"
-		self.root.text = msg
-		return super().on_touch_down(touch)
+		self.btn.text = msg
 
 	def waiting(self, delay):
-		self.root.text = "Go on, hit me!"
+		self.btn.text = "Go on, hit me!"
 
 if __name__ == "__main__":
 	HelloApp().run()
